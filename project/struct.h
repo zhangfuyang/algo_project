5024db3f59e667a178ad2b589936c80f0c47b52b
Wuyiheng <wuyiheng19961201@126.com>
2017-05-23 11:44:05 +0800

    Wuyiheng: add some annotation in struct.h

--- project/struct.h
+++ project/struct.h
@@ -7,31 +7,33 @@
 		dist = sqrt((type1.location.x - type2.location.x)*(type1.location.x - type2.location.x) + (type1.location.y - type2.location.y)*(type1.location.y - type2.location.y)); \
 	} while(0) 
 
+//计算两点间距离的宏函数
+
 extern int C;
 
-struct Location {
+struct Location {            //坐标结构体
 	int x;
 	int y;
 };
 
-struct Restaurant {
+struct Restaurant {          //餐厅位置结构体
 	Location location;
 };
 
-struct District {
+struct District {            //小区位置结构体
 	Location location;
 };
 
-struct Order {
+struct Order {               //订单具体信息：下单时间、餐厅id、小区id
 	int time;
 	int rid;
 	int did;
 };
 
-struct Cavalier {
+struct Cavalier {            //骑手具体信息：当前位置的坐标信息、当前的时刻、当前的状态（三个列表中处于哪个列表）、身上的订单信息、链接结构体
 	Location location;
 	int now;			//current time
-	int status;			
+	int status;			//free or full or not full
 	Order *order = new Order[C + 1];
 	LIST_ENTRY(Cavalier) cav_link;
 };
