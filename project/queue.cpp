e12e8ec6b165ec449302791d3064d4eb02347cf8
Wuyiheng <wuyiheng19961201@126.com>
2017-05-24 20:12:37 +0800

    Wu Yiheng coded Order and  Insert_order

--- project/queue.cpp
+++ project/queue.cpp
@@ -121,16 +121,124 @@ void find_free_cavalier(Order *order, Cavalier *cav)  //当Free列表不为空时，给Fr
 }
 
 
-float Insert_order(Order *order, Station_list head) {           //返回插入后的最大时间
+float Insert_order(Order *order, Station_list *head) {           //返回插入后的最大时间
 	Station *var = NULL;
-	int x1 = restaurant[order->rid].location.x;
+	Station *choose = NULL;
+	Station *newstation = NULL;
+	int x1 = restaurant[order->rid].location.x;         //订单的餐厅坐标            
 	int y1 = restaurant[order->rid].location.y;
-	int x2 = restaurant[order->did].location.x;
-	int y2 = restaurant[order->did].location.y;
+	int x2 = district[order->did].location.x;		    //订单的小区坐标
+	int y2 = district[order->did].location.y;
+	float distance;
+	float dist1;
+	float dist2;
+	float mindis = 10000;              //取最短距离变量
+	float delaytime;                   //插入后修改后面的station所用的延迟时间
+	float T;                           //返回的插入后的瓶颈时间
 
 
+	LIST_FOREACH(var, head, station_link)             //取得餐厅插入位置         
+	{
+		if ((LIST_NEXT(var, station_link)) != NULL) {
+			DISTANCE((*var), restaurant[order->rid], dist1);
+			DISTANCE((*(LIST_NEXT(var, station_link))), restaurant[order ->rid], dist2);
+			distance = dist1 + dist2;
+		}
+		if (distance < mindis) {
+			mindis = distance;
+			choose = var;
+		}
+	}
+
+	newstation = new Station[1];           
+
+	DISTANCE((*choose), restaurant[order->rid], dist1);
+
+	newstation->arrivetime = choose->leavetime + dist1;         //更新新插入餐厅的各项信息
+	if (newstation->arrivetime < order->time) {
+		newstation->leavetime = order->time;
+	}
+	else {
+		newstation->leavetime = newstation->arrivetime;
+	}
+	newstation->location.x = x1;
+	newstation->location.y = y1;
+	newstation->oid = order->orderid;
+	newstation->type = RESTAURANT;
+
+	LIST_INSERT_AFTER(choose, newstation, station_link);       //插入
+
+	var = LIST_NEXT(newstation, station_link);
+
+	DISTANCE((*var), restaurant[order->rid], dist2);
+
+	delaytime = newstation->leavetime + dist2 - var->arrivetime;      //延迟时间
 
 
+	LIST_FOREACH_FROM(var, head, station_link)
+	{
+		var->arrivetime = var->arrivetime + delaytime;
+		if (var->leavetime >= var->arrivetime) {
+			break;
+		}
+		else {
+			delaytime = var->arrivetime - var->leavetime;      //更新delaytime
+			var->leavetime = var->arrivetime;
+		}
+	}
+
+	var = newstation;           //为插入小区做准备
+	mindis = 10000;
+
+	LIST_FOREACH_FROM(var, head, station_link)            //从已经插入的餐厅之后再遍历
+	{
+		if ((LIST_NEXT(var, station_link)) != NULL) {
+			DISTANCE((*var), district[order->did], dist1);
+			DISTANCE((*(LIST_NEXT(var, station_link))), district[order->did], dist2);
+			distance = dist1 + dist2;
+		}
+		if (distance < mindis) {
+			mindis = distance;
+			choose = var;
+		}
+	}
+
+	newstation = new Station[1];
+
+	DISTANCE((*choose), district[order->did], dist1);
+
+	newstation->arrivetime = choose->leavetime + dist1;      //更新新插入小区的各项信息
+
+	newstation->leavetime = newstation->arrivetime;
+	
+	newstation->location.x = x2;
+	newstation->location.y = y2;
+	newstation->oid = order->orderid;
+	newstation->type = DISTRICT;
+
+	LIST_INSERT_AFTER(choose, newstation, station_link);     //插入小区
+
+	var = LIST_NEXT(newstation, station_link);
+
+	DISTANCE((*var), district[order->did], dist2);
+
+	delaytime = newstation->leavetime + dist2 - var->arrivetime;
+
+
+	LIST_FOREACH_FROM(var, head, station_link)             //更新插入小区之后的
+	{
+		var->arrivetime = var->arrivetime + delaytime;
+		if (var->leavetime >= var->arrivetime) {
+			break;
+		}
+		else {
+			delaytime = var->arrivetime - var->leavetime;
+			var->leavetime = var->arrivetime;
+		}
+	}
+
+	T = cal_bottlenecktime(*head);
+	return T;
 }
 
 
